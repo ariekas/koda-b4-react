f7f3007d9ae7209c680a3824c237d050abd0e586
ariekas <ariariekas@gmail.com>
2025-10-03 10:24:04 +0700

    fix: fixing wrong input Outlet

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,10 +1,10 @@
+import { Outlet } from "react-router-dom"
 import { TitleAuth } from "../TitleAuth"
-import { LoginPage } from "../../page/LoginPage"
 export function AuthLayout(){
     return(
         <>
         <TitleAuth/>
-        <LoginPage/>
+        <Outlet/>
         </>
     )
 }
