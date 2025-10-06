e5704eebc3246c928ca0d7ca3c3d807b9882debf
ariekas <ariariekas@gmail.com>
2025-10-06 15:01:41 +0700

    feat: creating sidebar filtered product

--- src/page/ProductPage.jsx
+++ src/page/ProductPage.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
 import { CardMenu } from "../components/CardMenu"
 import { Icon } from "../components/Icon"
+import { Button } from "../components/Button"
 export function ProductPage() {
     const arr = [1, 2]
+    const [showFilter, setShowFilter] = useState(false)
+    const [priceRange, setPriceRange] = useState([0, 100000])
+
+    function toggleFilter() {
+        setShowFilter(!showFilter)
+    }
+
+    function handleRangePrice(e) {
+        const value = Number(e.target.value)
+        setPriceRange([10000, value])
+    }
     return (
         <>
             <div className="pt-22 p-5">
@@ -13,11 +26,115 @@ export function ProductPage() {
                         <input type="text" className="border p-3 rounded-lg border-gray-300 pl-8 w-full text-sm" placeholder="Find Product" />
                     </div>
                     <div className="p-3 flex items-center bg-[#FF8906] rounded-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-                            <path fill="#000" d="M22 18.605a.75.75 0 0 1-.75.75h-5.1a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h7.74a2.93 2.93 0 0 1 5.66 0h5.1a.75.75 0 0 1 .75.75m0-13.21a.75.75 0 0 1-.75.75H18.8a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h10.39a2.93 2.93 0 0 1 5.66 0h2.45a.74.74 0 0 1 .75.75m0 6.6a.74.74 0 0 1-.75.75H9.55a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h1.14a2.93 2.93 0 0 1 5.66 0h11.7a.75.75 0 0 1 .75.75" stroke-width="1" stroke="#000" />
-                        </svg>
+                        {/* button side bar */}
+                        <button onClick={toggleFilter}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                                <path fill="#000" d="M22 18.605a.75.75 0 0 1-.75.75h-5.1a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h7.74a2.93 2.93 0 0 1 5.66 0h5.1a.75.75 0 0 1 .75.75m0-13.21a.75.75 0 0 1-.75.75H18.8a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h10.39a2.93 2.93 0 0 1 5.66 0h2.45a.74.74 0 0 1 .75.75m0 6.6a.74.74 0 0 1-.75.75H9.55a2.93 2.93 0 0 1-5.66 0H2.75a.75.75 0 1 1 0-1.5h1.14a2.93 2.93 0 0 1 5.66 0h11.7a.75.75 0 0 1 .75.75" stroke-width="1" stroke="#000" />
+                            </svg>
+                        </button>
                     </div>
                 </div>
+
+
+                <div
+                    className={`fixed inset-0 bg-black transition-opacity duration-300 z-5
+                 ${showFilter ? "opacity-20 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+                    onClick={() => setShowFilter(false)}
+                ></div>
+                <div
+                    className={`fixed top-0 right-0 h-full w-64 bg-[#0B0909] shadow-sm z-10 transform transition-transform duration-300
+                ${showFilter ? "translate-x-0" : "translate-x-full"}`}
+                >
+                    <div className="p-5 flex flex-col gap-4 text-white">
+                        <div className=" flex justify-between items-center">
+                            <h2 className="text-lg font-semibold text-white">Filter</h2>
+                            <button onClick={() => setShowFilter(false)}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        fill="none"
+                                        stroke="#fff"
+                                        strokeWidth="2"
+                                        d="M6 6l12 12M6 18L18 6"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="" className="font-semibold">Search</label>
+                            <input type="search" className="py-3 bg-white text-black text-sm p-3 rounded-md" placeholder="Search Your Product" />
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <label htmlFor="" className="font-bold">Category</label>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Favorite Product</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Coffee</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Non Coffee</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Foods</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Add-On</label>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                            <label htmlFor="" className="font-bold">Sort By</label>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Buy 1 get 1</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Flash sale</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Birthday Package</label>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <input type="checkbox" className="w-4 h-4" />
+                                <label htmlFor="" className="text-sm font-light">Cheap</label>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-3 mt-5">
+                            <label className="font-bold">Price Range</label>
+                            <div className="flex justify-between text-sm">
+                                <span>Rp {priceRange[0].toLocaleString()}</span>
+                                <span>Rp {priceRange[1].toLocaleString()}</span>
+                            </div>
+
+                            <input
+                                type="range"
+                                min="10000"
+                                max="100000"
+                                step="1000"
+                                value={priceRange[1]}
+                                onChange={handleRangePrice}
+                                className="w-full accent-yellow-400 cursor-pointer"
+                            />
+                        </div>
+
+                        <Button style={" bg-[#FF8906] flex justify-center "}>
+                            Apply Filter
+                        </Button>
+                    </div>
+                </div>
+
                 <div className="flex flex-col gap-6 mt-4">
                     <h1 className="text-2xl font-semibold">Today <span className="text-[#8E6447]">Promo</span></h1>
                     <div className="flex items-center overflow-x-auto gap-4">
